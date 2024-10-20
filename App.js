c91bbebc65a74ca8944b232d4e247fa0d073e464
ManahilHabibb <manahil5503@gmail.com>
2024-10-20 11:21:21 +0500

    updated

--- App.js
+++ App.js
@@ -1,31 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-
+import { StyleSheet, View, Text ,Image , ImageBackground} from 'react-native';
 
 export default function App() {
-
   return (
-    
-    <View style={styles.container}>
-   <View style={styles.space}></View>
-      <StatusBar style={styles.statusbar} />
-      <Foot/>
-    </View>
+    <ImageBackground style={styles.backgroud} source={require('./assets/bg.png')}>
+      <Image style={styles.img} source={require('./assets/image.png')}/>
+    </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  
-    
+  backgroud:{
+     
+      resizeMode: 'cover',
+      justifyContent: 'center',
+      flex:1,
+      
   },
-  statusbar: {
-    backgroundColor: 'blue',
-    
-  },
-  space: {
-    height:680 ,
+
+  img:{
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginBottom: 100,
   }
+  
 });
+
+
--- function.js
+++
@@ -1,6 +0,0 @@
-function myfunction(name,message)
-{
-    return  "Hi" +name +" "+ message;
-}
-
-console.log(myfunction("manahil","welcome"));
